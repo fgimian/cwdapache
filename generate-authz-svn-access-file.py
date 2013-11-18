ceab096d0d95342e50cf2fd4a60d31c4b2fa9595
Joseph Walton <jwalton@atlassian.com>
2013-11-18 12:30:25 +1100

    Process an existing svn authorization file.
    Implement the same behaviour as the current Crowd module;
    accept a template and expand the groups section with memberships
    from Crowd.

--- generate-authz-svn-access-file.py
+++ generate-authz-svn-access-file.py
@@ -2,15 +2,25 @@
 
 # Generate a mod_authz_svn AuthzSVNAccessFile with memberships from a Crowd server
 
+# Provide an existing access file and the groups section will be expanded
+#  with the memberships defined in Crowd.
+
 import json
 from httplib2 import Http
 from urllib.parse import quote
 
-from sys import stderr
+from sys import stderr, argv, exit
+
+import re
 
 # Crowd deployment base URL
 base = 'http://jwalton-desktop:4990/crowd'
 um = base + '/rest/usermanagement/1'
+
+# Parse command-line arguments
+if len(argv) > 2:
+  print('Usage: %s [access-file]' % argv[0])
+  exit(5)
 
 http = Http(cache = '.cache')
 
@@ -26,12 +36,44 @@ def get(url):
     exit(10)
   return json.loads(content.decode('utf-8'))
  
+def membersOf(groupName):
+  return [user['name'] for user in get(um + '/group/user/nested?groupname=' + quote(groupName))['users']]
+
 print('# Membership from %s' % base)
-print('[groups]')
 
-# Get all groups
-for groupName in sorted([group['name'] for group in get(um + '/search?entity-type=GROUP&restriction=')['groups']]):
-  # And their members
-  names = [user['name'] for user in get(um + '/group/user/nested?groupname=' + quote(groupName))['users']]
+groupLine = re.compile('([^#][^=]+)\s*=\s*')
 
-  print('%s = %s' % (groupName, ', '.join(sorted(names))))
+shownGroups = False
+
+# If a file was specified, process it and expand the groups section
+if len(argv) == 2:
+  with open(argv[1]) as cfg:
+    inGroups = False
+    for l in [l.rstrip('\r\n') for l in cfg]:
+      if inGroups:
+        m = groupLine.match(l)
+        if m:
+          groupName = m.group(1)
+          print('%s = %s' % (groupName, ', '.join(sorted(membersOf(groupName)))))
+        else:
+          print(l)
+      else:
+        print(l)
+
+      if l == '[groups]':
+        inGroups = True
+        shownGroups = True
+      elif l.startswith('['):
+        inGroups = False
+
+        print('here', l)
+
+# If there was no groups section, create one with all memberships
+if not shownGroups:
+  print()
+  print('[groups]')
+
+  for groupName in sorted([group['name'] for group in get(um + '/search?entity-type=GROUP&restriction=')['groups']]):
+    # And their members
+    names = membersOf(groupName)
+    print('%s = %s' % (groupName, ', '.join(sorted(names))))
